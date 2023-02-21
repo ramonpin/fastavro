7b513c819b2dccb91be6c7212914edba807f6c57
ramonpin <ramon.pin@gmail.com>
2023-02-21 12:55:51 +0100

    test: Use setup blocks and better fun chaining

--- test/fastavro_errors_test.exs
+++ test/fastavro_errors_test.exs
@@ -33,48 +33,61 @@ defmodule FastavroErrorsTest do
 
   @avro_person <<8, 108, 117, 105, 115, 50, 0, 0, 0, 0, 0, 0, 30, 64>>
 
+  setup do
+    {:ok, incompatible_person_schema} = FastAvro.read_schema(@incompatible_person_schema)
+    {:ok, person_schema} = FastAvro.read_schema(@person_schema)
+
+    %{person_schema: person_schema, incompatible_person_schema: incompatible_person_schema}
+  end
+
   test "A wrong schema gives an error" do
     {:error, reason} = FastAvro.read_schema("{}")
 
     assert reason == :bad_avro_schema
   end
 
-  test "encode with an incompatible schema gives error" do
-    {:ok, schema} = FastAvro.read_schema(@incompatible_person_schema)
+  test "encode with an incompatible schema gives error", %{
+    incompatible_person_schema: schema
+  } do
     {:error, reason} = FastAvro.encode_avro_datum(@map_person, schema)
 
     assert reason == :incompatible_avro_schema
   end
 
-  test "decode with an incompatible schema gives error" do
-    {:ok, schema} = FastAvro.read_schema(@incompatible_person_schema)
+  test "decode with an incompatible schema gives error", %{
+    incompatible_person_schema: schema
+  } do
     {:error, reason} = FastAvro.decode_avro_datum(@avro_person, schema)
 
     assert reason == :incompatible_avro_schema
   end
 
-  test "get unknown field gives an error" do
-    {:ok, schema} = FastAvro.read_schema(@person_schema)
+  test "get unknown field gives an error", %{
+    person_schema: schema
+  } do
     {:ok, record} = FastAvro.decode_avro_datum(@avro_person, schema)
     assert {:error, :field_not_found} = FastAvro.get_avro_value(record, "wrong")
   end
 
-  test "get raw value with an incompatible schema gives error" do
-    {:ok, schema} = FastAvro.read_schema(@incompatible_person_schema)
+  test "get raw value with an incompatible schema gives error", %{
+    incompatible_person_schema: schema
+  } do
     {:error, reason} = FastAvro.get_raw_value(@avro_person, schema, "name")
 
     assert reason == :incompatible_avro_schema
   end
 
-  test "get value from unknown field gives an error" do
-    {:ok, schema} = FastAvro.read_schema(@person_schema)
+  test "get value from unknown field gives an error", %{
+    person_schema: schema
+  } do
     {:ok, avro_record} = FastAvro.decode_avro_datum(@avro_person, schema)
 
     assert {:error, :field_not_found} = FastAvro.get_avro_value(avro_record, "wrong")
   end
 
-  test "get raw value for unknown field gives an error" do
-    {:ok, schema} = FastAvro.read_schema(@person_schema)
+  test "get raw value for unknown field gives an error", %{
+    person_schema: schema
+  } do
     assert {:error, :field_not_found} = FastAvro.get_raw_value(@avro_person, schema, "wrong")
   end
 end
